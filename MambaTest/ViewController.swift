7fe59e2376caa4a21d57f37565df166f7660379b
0atme0 <0atme0@gmail.com>
2017-07-16 20:12:33 +0300

    Initial Commit

---
+++ MambaTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MambaTest
+//
+//  Created by Andrey Ildyakov on 16.07.17.
+//  Copyright © 2017 Andrey Ildyakov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
